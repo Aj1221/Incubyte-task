a9662469444665ae12a90f45b59ca87265a5582d
dentalsem-fedev <aj.dude@rediff.com>
2024-09-24 21:04:35 +0530

    stringCalculator code written

--- src/utils/stringCalculator.js
+++ src/utils/stringCalculator.js
@@ -1,7 +1,7 @@
 export function add(numbers = "") {
     if (numbers === "") return 0;
 
-    // Check if custom delimiter is provided
+    //This is to Check if custom delimiter is provided
     let delimiter = /,|\n/;
     if (numbers.startsWith("//")) {
         const delimiterMatch = numbers.match(/^\/\/(.+)\n/);
@@ -11,6 +11,14 @@ export function add(numbers = "") {
         }
     }
 
-  
+    //This is to Split based on delimiter
+    const numberArray = numbers.split(delimiter).map(Number);
+
+    //This is to Check for negative numbers
+    const negativeNumbers = numberArray.filter(num => num < 0);
+    if (negativeNumbers.length > 0) {
+        throw new Error(`Negative numbers not allowed: ${negativeNumbers.join(", ")}`);
+    }
+
     return numberArray.reduce((acc, curr) => acc + curr, 0);
 }
